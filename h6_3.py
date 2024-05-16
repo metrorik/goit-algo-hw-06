5a605fb4cc50726e7c40ddcb35bb62546170561d
metrorik <58214662+metrorik@users.noreply.github.com>
2024-05-16 15:25:35 +0200

    Update h6_3.py

--- h6_3.py
+++ h6_3.py
@@ -1,5 +1,4 @@
 import networkx as nx
-import matplotlib.pyplot as plt
 
 # порожній граф
 G = nx.Graph()
@@ -16,35 +15,48 @@ edges = [
 ]
 G.add_weighted_edges_from(edges)
 
-# Візуалізація графа
-plt.figure(figsize=(10, 7))
-pos = nx.spring_layout(G)  # Розташування вузлів
-nx.draw(G, pos, with_labels=True, node_color="skyblue", node_size=1500, edge_color="gray", font_size=15)
-edge_labels = nx.get_edge_attributes(G, 'weight')
-nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
-plt.title("Транспортна мережа міста з вагами")
-plt.show()
 
-# алгоритм Дейкстри для знаходження найкоротшого шляху
-def dijkstra_all_pairs_shortest_paths(graph):
-    return dict(nx.all_pairs_dijkstra_path(graph))
+# Реалізація алгоритму Дейкстри
+def dijkstra(graph, start):
+    distances = {vertex: float('infinity') for vertex in graph}
+    distances[start] = 0
+    unvisited = list(graph.keys())
+    
+    while unvisited:
+        current_vertex = min(unvisited, key=lambda vertex: distances[vertex])
+        
+        if distances[current_vertex] == float('infinity'):
+            break
+        
+        for neighbor, weight in graph[current_vertex].items():
+            distance = distances[current_vertex] + weight
+            
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+        
+        unvisited.remove(current_vertex)
+    
+    return distances
 
-# найкоротші шляхи між усіма вершинами
-shortest_paths = dijkstra_all_pairs_shortest_paths(G)
 
-# аналіз основних характеристик
-num_nodes = G.number_of_nodes()
-num_edges = G.number_of_edges()
-degree_dict = dict(G.degree())
-average_degree = sum(degree_dict.values()) / num_nodes
+# перетворення формату
+graph_dict = {node: {} for node in G.nodes}
+for edge in G.edges(data=True):
+    u, v, data = edge
+    weight = data['weight']
+    graph_dict[u][v] = weight
+    graph_dict[v][u] = weight
 
-print(f"Кількість вершин: {num_nodes}")
-print(f"Кількість ребер: {num_edges}")
-print(f"Ступінь вершин: {degree_dict}")
-print(f"Середній ступінь вершин: {average_degree:.2f}")
 
-# вивід найкоротших шляхів між усіма вершинами
-for start_node in shortest_paths:
-    for end_node in shortest_paths[start_node]:
-        path = shortest_paths[start_node][end_node]
-        print(f"Найкоротший шлях від {start_node} до {end_node}: {path}")
+# найкоротші шляхи від усіх вершин
+all_shortest_paths = {}
+for node in graph_dict:
+    distances = dijkstra(graph_dict, node)
+    all_shortest_paths[node] = distances
+
+
+print("\nНайкоротші шляхи між усіма вершинами:")
+for start_node in all_shortest_paths:
+    for end_node in all_shortest_paths[start_node]:
+        path_distance = all_shortest_paths[start_node][end_node]
+        print(f"Найкоротший шлях від {start_node} до {end_node} має довжину: {path_distance}")
